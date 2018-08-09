e7f03e0fc97ab25ee6fc5501306885bc4c467e45
Thomas Russell <thomasbeckrussell@gmail.com>
2018-08-09 17:11:14 -0500

    Finished JS to search, pull thumbnails, title, author, and description.  Also added link to view the video

--- search.js
+++ search.js
@@ -2,19 +2,33 @@ const  YOUTUBE_SEARCH_URL = 'https://www.googleapis.com/youtube/v3/search';
 
 function getData(searchTerm, callback){
     const query = {
-        part: `${searchTerm} in: name`,
+        part: 'snippet',
+        q : searchTerm,
         key: APIkey,
-        maxResults: 50
+        maxResults: 25
     }
     $.getJSON(YOUTUBE_SEARCH_URL,query,callback);
 }
-function renderResult(results){
+function renderResult(result){
+    let datePublished = Date.now() - result.snippet.publishedAt;
     return `
-        <div>
-            <h2>
-            <a class="js-result-name" href="${result.html_url}" target="_blank">${result.name}</a>
-            </h2>
-        </div>
+        <div class = 'js-search-result'>
+                            <a href="https://www.youtube.com/watch?v=${result.id.videoId}">
+                                <img src= ${result.snippet.thumbnails.high.url} alt="name of video">
+                                <div class="video-details">
+                                    <h3>${result.snippet.title}</h3>
+                                    <ul>
+                                        <li>${result.snippet.channelTitle}</li>
+                                        <li>40K? views</li>
+                                        <li>${result.snippet.publishedAt}</li>
+                                    </ul>
+                                    <p class="video-description">
+                                        ${result.snippet.description}
+                                    </p>
+                                </div>
+                            </a>
+                        </div>  
+                <div>
     `
 }
 
